b63903519677dc7f15dd654d41a35db870c65c2e
Rossovr <vrojas8b2014@gmail.com>
2026-06-14 10:37:53 +0200

    Add files via upload

---
+++ data.js
@@ -0,0 +1,358 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-palimpsest-",
+      "name": "Palimpsest ",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.09279816356205473,
+        "pitch": 0.20543500632405198,
+        "fov": 1.4324874404875778
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.24074518863722005,
+          "pitch": 0.3398953374874871,
+          "rotation": 0,
+          "target": "1-vista-frontal-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.6326761018040568,
+          "pitch": -0.07226087901283051,
+          "title": "Palimpsest",
+          "text": "Palimpsest propone la reutilización de materiales de construcción y demolición como estrategia de diseño circular para la rehabilitación de interiores, transformando recursos recuperados en nuevos elementos arquitectónicos a través de una intervención demostrativa."
+        }
+      ]
+    },
+    {
+      "id": "1-vista-frontal-",
+      "name": "Vista Frontal ",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.3820746810734281,
+          "pitch": 0.2710820582145068,
+          "rotation": 0,
+          "target": "0-palimpsest-"
+        },
+        {
+          "yaw": -0.09852538340509831,
+          "pitch": 0.33622186475606597,
+          "rotation": 0,
+          "target": "2-habitacin"
+        },
+        {
+          "yaw": -3.039703007235177,
+          "pitch": 0.24533194701660932,
+          "rotation": 0,
+          "target": "3-vista-exterior-frontal"
+        },
+        {
+          "yaw": 1.4466695646274,
+          "pitch": 0.2809285988550112,
+          "rotation": 0,
+          "target": "4-vista-escaleras"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-habitacin",
+      "name": "Habitación",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.5961310568381224,
+          "pitch": 0.4659710444831173,
+          "rotation": 0,
+          "target": "5-bao-"
+        },
+        {
+          "yaw": 3.0720408485437964,
+          "pitch": 0.473690020946421,
+          "rotation": 0,
+          "target": "1-vista-frontal-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-vista-exterior-frontal",
+      "name": "Vista Exterior Frontal",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.13209554434297566,
+          "pitch": 0.2519444750153852,
+          "rotation": 0,
+          "target": "1-vista-frontal-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-vista-escaleras",
+      "name": "Vista Escaleras",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.1860677606549483,
+          "pitch": 0.23179473052405086,
+          "rotation": 0,
+          "target": "1-vista-frontal-"
+        },
+        {
+          "yaw": 2.9144137004420845,
+          "pitch": 0.07460302316579082,
+          "rotation": 0,
+          "target": "6-vista-piscina"
+        },
+        {
+          "yaw": -0.16543498297853176,
+          "pitch": 0.3355082587567111,
+          "rotation": 0,
+          "target": "7-bodega"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-bao-",
+      "name": "Baño ",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.1087408707479227,
+          "pitch": 0.5382842339703178,
+          "rotation": 0,
+          "target": "2-habitacin"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-vista-piscina",
+      "name": "Vista Piscina",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.5733648754658809,
+          "pitch": 0.2291429436738106,
+          "rotation": 0,
+          "target": "4-vista-escaleras"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-bodega",
+      "name": "Bodega",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.1220655973257507,
+          "pitch": 0.49710584327364415,
+          "rotation": 0,
+          "target": "4-vista-escaleras"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
